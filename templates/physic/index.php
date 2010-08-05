44dd80c17af4632258266e294f7d97cb9b211607
derzkiy <kinotimebox@gmail.com>
2010-08-06 02:12:15 +0600

    маятник Signed-off-by: derzkiy <kinotimebox@gmail.com>

---
+++ templates/physic/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>PhysicBox</title>
+
+<link rel="stylesheet" href="reset.css" type="text/css" />
+<link rel="stylesheet" href="style.css" type="text/css" />
+
+</head>
+
+<body>
+
+	<div id="box">
+    
+    	<div id="left-menu">
+        	<h3>Пружинный маятник</h3>
+            <ul>
+            	<li><a href="">Теория</a></li>
+                <li><a href="">Практика</a></li>
+                <li><a href="">Тесты</a></li>
+            </ul>
+            
+        </div>
+    
+    	<div id="board-top">
+        </div>
+        <div id="board-fon">
+        
+        	<div id='content'>
+            	<div style='width:160px; float:left;'>
+                	<p style="margin-bottom:7px;">Жесткость пружины, k = 1.5</p>
+                    <p style='margin-bottom:30px;'>Вес груза, m = 1 кг</p>
+                    <div id="pillar">
+                    </div>
+                    <div id="spring">
+                    </div>
+                    <div id="load">
+                    </div>
+                </div>
+                
+                <div style="width:300px; float:left; margin-left:50px;">
+                	<p style="margin-bottom:18px;">Пружинный маятник — механическая система, состоящая из пружины с коэффициентом упругости (жёсткостью) k (закон Гука), один конец которой жёстко закреплён, а на втором находится груз массы m.</p>
+					<p style="margin-bottom:18px;">Когда на массивное тело действует упругая сила, возвращающая его в положение равновесия, оно совершает колебания около этого положения.Такое тело называют пружинным маятником.</p>
+                    <div id="graph">
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+        <div id="board-handle">
+        </div> 
+    
+    </div>
+
+</body>
+</html>
